03218b77cd9d0b1a2c628321d2ed9dd727e2bb82
Max <maxzuba22@yahoo.co.uk>
2016-07-06 16:34:24 +0100

    Create summon.user.js

---
+++ summon.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name Auto Summoner
+// @namespace http://joker314.github.io
+// @version 1.1
+// @description  addresses a comment reply to anybody you want on Scratch
+// @match *://scratch.mit.edu/*
+// ==/UserScript==
